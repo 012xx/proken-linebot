ac5346369e3c5b3afb973e92274e110ce16fcc56
012xx <night.fox.045@gmail.com>
2022-04-04 18:01:04 +0900

    update:Deploy-linebot

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const config: Config = {
   channelAccessToken: functions.config().line.channel_access_token,
   channelSecret: functions.config().line.channel_secret,
 };
+
 const client = new line.Client(config);
 
 export const lineWebhook = functions
